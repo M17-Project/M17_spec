b78332d64482b7036444f2b566892ad6aeb6c6b7
kc1awv <smiller@kc1awv.net>
2021-01-25 11:13:37 -0500

    testing latex

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ htmlhelp_basename = 'M17ProtocolSpecificationdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_engine = 'xelatex'
+latex_engine = 'latex'
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
